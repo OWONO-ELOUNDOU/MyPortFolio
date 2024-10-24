7fbcc6cb5a1661603ebdff515fe4df78b3f07fef
OWONO ELOUNDOU <OWONO ELOUNDOU>
2024-10-24 23:45:45 +0100

    Home page review, about page implementer, work page added

---
+++ src/app/Pages/works/works.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-works',
+  standalone: true,
+  imports: [],
+  templateUrl: './works.component.html',
+  styleUrl: './works.component.sass'
+})
+export class WorksComponent {
+
+}
